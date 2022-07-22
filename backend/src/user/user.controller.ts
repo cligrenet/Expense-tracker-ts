c2aca5138d860179896f6c73d82c3a550345c683
Chenjia Li <cligrenet@gmail.com>
2022-07-22 15:52:32 +0200

    Complete user related features

--- backend/src/user/user.controller.ts
+++ backend/src/user/user.controller.ts
@@ -1,10 +1,15 @@
-import { Controller, Get, Patch, UseGuards } from '@nestjs/common';
+import { Body, Controller, Get, Patch, UseGuards } from '@nestjs/common';
 import { User } from '@prisma/client';
 import { GetUser } from '../auth/decorator';
 import { JwtGuard } from '../auth/guard';
+import { EditUserDto } from './dto';
+import { UserService } from './user.service';
 
+@UseGuards(JwtGuard)
 @Controller('users')
 export class UserController {
+  constructor(private userService: UserService) {} // import userService through dependency injection
+
   @UseGuards(JwtGuard)
   @Get('me') // baseUrl/users/me
   getMe(@GetUser() user: User) {
@@ -12,5 +17,7 @@ export class UserController {
   }
 
   @Patch()
-  editUser() {}
+  editUser(@GetUser('id') userId: number, @Body() dto: EditUserDto) {
+    return this.userService.editUser(userId, dto);
+  }
 }
