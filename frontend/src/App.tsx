2fce43e3840745efdfe7bfc4e602d36f1773d149
Chenjia Li <cligrenet@gmail.com>
2022-07-26 18:03:33 +0200

    Add protected routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -7,17 +7,25 @@ import Home from './pages/Home';
 import Income from './pages/Income';
 import Login from './pages/Login';
 import Signup from './pages/Signup';
+import PrivateRoute from './components/PrivateRoute';
 
 function App() {
 	return (
 		<>
 			<Router>
 				<Routes>
-					<Route path="/" element={<Home />} />
-					<Route path="/income" element={<Income />} />
-					<Route path="/expense" element={<Income />} />
-					<Route path="/auth/login" element={<Login />} />
-					<Route path="/auth/signup" element={<Signup />} />
+					<Route path="/" element={<PrivateRoute />}>
+						<Route path="/" element={<Home />} />
+					</Route>
+					<Route path="/" element={<PrivateRoute />}>
+						<Route path="/income" element={<Income />} />
+					</Route>
+					<Route path="/" element={<PrivateRoute />}>
+						<Route path="/expense" element={<Income />} />
+					</Route>
+
+					<Route path="/login" element={<Login />} />
+					<Route path="/signup" element={<Signup />} />
 				</Routes>
 			</Router>
 			<ToastContainer newestOnTop />
