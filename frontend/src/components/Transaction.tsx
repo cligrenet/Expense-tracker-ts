2dbfda29ca69061fedbe9dd58ea4396aab7dcce3
Chenjia Li <cligrenet@gmail.com>
2022-07-26 18:01:13 +0200

    Add transaction and transactionlist components

---
+++ frontend/src/components/Transaction.tsx
@@ -0,0 +1,52 @@
+import { useContext, useEffect } from 'react';
+import { GlobalContext } from '../context/GlobalState';
+import { numberWithCommas } from '../utils/format';
+import { motion } from 'framer-motion';
+import { FaRegTrashAlt } from 'react-icons/fa';
+import { toast } from 'react-toastify';
+import { Transaction as TransactionType } from '../context/GlobalState';
+
+const Transaction = ({ transaction }: { transaction: TransactionType }) => {
+	const { user, deleteTransaction, isTransactionsError, transactionsError } = useContext(GlobalContext);
+
+	const sign = transaction.amount > 0 ? '+' : '-';
+	const listItem =
+		'flex flex-wrap justify-between items-center py-2 px-3 rounded-lg my-2 bg-muted-darkblue hover:bg-yellow text-slate-300 hover:text-darkblue-1 hover:border-yellow border-l-4 ';
+	const plus = 'border-l-4 border-green';
+	const minus = 'border-l-4 border-red';
+
+	useEffect(() => {
+		if (isTransactionsError) {
+			toast.error(transactionsError);
+		}
+	}, [isTransactionsError, transactionsError]);
+
+	return (
+		<motion.li
+			className={transaction.amount > 0 ? listItem + plus : listItem + minus}
+			// transaction={transaction}
+			initial={{ opacity: 0 }}
+			animate={{ opacity: 1 }}
+			exit={{ opacity: 0 }}
+		>
+			<span className="flex flex-wrap flex-col">
+				<span>{transaction.text}</span>
+				<span className="text-xs">{transaction.createdAt!.split('T')[0]}</span>
+			</span>
+
+			<span>
+				<span>
+					{sign} €{numberWithCommas(Math.abs(transaction.amount))}
+				</span>
+				<button
+					className="pl-3 hover:text-red"
+					onClick={() => deleteTransaction(transaction.id!, user.access_token)}
+				>
+					<FaRegTrashAlt />
+				</button>
+			</span>
+		</motion.li>
+	);
+};
+
+export default Transaction;
---
+++ frontend/src/components/TransactionList.tsx
@@ -0,0 +1,136 @@
+import { useContext, useEffect, useState } from 'react';
+import { GlobalContext } from '../context/GlobalState';
+import Transaction from './Transaction';
+import { useLocation } from 'react-router-dom';
+import { AnimatePresence } from 'framer-motion';
+import Spinner from '../components/Spinner';
+import { toast } from 'react-toastify';
+import { FaFilter, FaListUl, FaSortAmountDown, FaSortAmountUpAlt } from 'react-icons/fa';
+import FiltersModal from './FiltersModal';
+// import { categories } from '../utils/categoriesData';
+
+const TransactionList = () => {
+	// States from context
+	const {
+		user,
+		transactions,
+		transactionsError,
+		isTransactionsError,
+		isTransactionsLoading,
+		getTransactions,
+		transactionsSortingDirection,
+		toggleTransactionSortDirection,
+		transactionsSelectedCategories,
+		handleTransactionsSelectedCategories,
+		// getIncomes,
+		// getExpenses,
+	} = useContext(GlobalContext);
+
+	const location = useLocation();
+	// console.log(location.pathname);
+
+	// Modal state
+	const [modalIsOpen, setModalIsOpen] = useState(false);
+
+	// Open/close modal
+	const openModal = () => {
+		setModalIsOpen(true);
+	};
+
+	const closeModal = () => {
+		setModalIsOpen(false);
+	};
+
+	useEffect(() => {
+		switch (location.pathname) {
+			case '/':
+				getTransactions(user.access_token);
+				break;
+			// case '/income':
+			// getIncomes(user.token);
+			// break;
+			// case '/expense':
+			// getExpenses(user.token);
+			// break;
+			default:
+				getTransactions(user.access_token);
+		}
+
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, [transactionsSelectedCategories, transactionsSortingDirection]); // Whenever selected categories change or sorting direction change, update transaction list
+
+	useEffect(() => {
+		if (isTransactionsError) {
+			toast.error(transactionsError);
+		}
+	}, [isTransactionsError, transactionsError]);
+
+	// Render all transactions (reset all filters)
+	const showAllTransactions = () => {
+		handleTransactionsSelectedCategories([]);
+	};
+
+	// Handle transaction list sorting (desc/asc)
+	const toggleSortTransactionsDate = () => {
+		toggleTransactionSortDirection();
+	};
+
+	// Open filters modal => Handle transaction list filtering (by week, by month, by year or by category)
+	const filterTransactions = () => {
+		openModal();
+	};
+
+	if (isTransactionsLoading) {
+		return <Spinner />;
+	}
+
+	return (
+		<>
+			<div className="flex flex-wrap justify-between items-center">
+				<h3 className="text-l text-purple mb-3">Transactions</h3>
+				<div className="flex flex-wrap">
+					<div
+						className="flex flex-nowrap items-center text-slate-300 text-xs hover:text-yellow cursor-pointer mr-3"
+						onClick={toggleSortTransactionsDate}
+					>
+						{transactionsSortingDirection === 'desc' ? (
+							<FaSortAmountDown className="mr-0.5" />
+						) : (
+							<FaSortAmountUpAlt className="mr-0.5" />
+						)}{' '}
+						Sort
+					</div>
+
+					<div
+						className="flex flex-nowrap items-center text-slate-300 text-xs hover:text-yellow cursor-pointer mr-3"
+						onClick={filterTransactions}
+					>
+						<FaFilter className="mr-0.5 text-[8px]" /> Filters
+					</div>
+
+					<div
+						className="flex flex-nowrap items-center text-slate-300 text-xs hover:text-yellow cursor-pointer "
+						onClick={showAllTransactions}
+					>
+						<FaListUl className="mr-0.5" /> All
+					</div>
+				</div>
+			</div>
+
+			{(!transactions || !transactions.length) && <p className="text-slate-300">Please add a transaction</p>}
+			{transactions && (
+				<ul className="list">
+					<AnimatePresence>
+						{transactions.map((transaction) => (
+							<Transaction key={transaction.id} transaction={transaction} />
+						))}
+					</AnimatePresence>
+				</ul>
+			)}
+
+			<FiltersModal modalIsOpen={modalIsOpen} closeModal={closeModal} />
+		</>
+	);
+};
+
+export default TransactionList;
